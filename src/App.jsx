4b4357b75833f8df7623b3e7611fbbdff740a7cf
not-ludwig <lokillo0012@gmail.com>
2024-05-06 19:03:15 -0400

    add image, add current date

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@ import './App.css'
 import Top10 from './Top10';
 
 function App() {
-   
+   const today = new Date();
+   let year = today.getFullYear();
+   let month = today.getMonth();
+   let day = today.getDay();
+
+   if (day < 10) day = '0' + day
+   if (month < 10) month = '0' + month
+
+
   return (  
     <>
       <main>
         <h1 className='main-title'> TOP 10 SONG in SPOTIFY TODAY</h1>
+        <h2 className='current-date'>{day}-{month}-{year}</h2>
         <Top10></Top10>
       </main>
     </>
